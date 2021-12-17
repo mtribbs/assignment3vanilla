ecdc342c55c3e8399fa09fe6065ed27f3e5f3af7
mtribbs <89516065+mtribbs@users.noreply.github.com>
2021-12-17 19:04:10 +0100

    basic

--- script.js
+++ script.js
@@ -1,1 +1,43 @@
-console.log("testing");
+
+//DOM elements
+
+const menuBtn = document.getElementById("menubtn");
+const dropDown = document.getElementById("dropdown");
+
+// hide dropdown menu
+
+dropDown.style.display = "none";
+
+// menu function
+
+function menuClosed() {
+    dropDown.style.display = "none";  
+}
+
+function menuOpen() {
+    dropDown.style.display = "flex";   
+}
+
+
+
+function openMenu() {
+    if (dropDown.style.display === "none") {
+        return menuOpen();
+    }
+    else {
+        return menuClosed();
+    }
+}
+
+// event listeners
+
+menuBtn.addEventListener("click", openMenu);
+
+
+document.addEventListener('click', function(event) {
+  const isClickInside = menuBtn.contains(event.target);
+
+  if (!isClickInside) {
+    return menuClosed();
+  }
+});
